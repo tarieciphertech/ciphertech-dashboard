b54cb5b4726c3b3e65ac470f3e43c629713b1a83
cyphertech <tariecipher@gmail.com>
2026-09-08 16:37:42 +0200

    Wire dedicated invitation setup route

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import SetupPassword from './auth/SetupPassword'
 import { AuthProvider } from './auth/AuthProvider'
 import './styles.css'
+
+function Root() {
+  return window.location.pathname === '/auth/setup-password' ? <SetupPassword /> : <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <Root />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,

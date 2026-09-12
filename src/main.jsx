36649a38afab412b3580cbbcd0e1f884f8bb5c5d
cyphertech <tariecipher@gmail.com>
2026-09-12 17:59:53 +0200

    Consolidate UI stylesheet loading

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,8 @@ import './styles.css'
 
 const brandStylesheet=document.createElement('link')
 brandStylesheet.rel='stylesheet'
-brandStylesheet.href='/branding.css?v=20260908-premium'
+brandStylesheet.href='/branding.css?v=20260912-system'
 document.head.appendChild(brandStylesheet)
-
-const uiPolishStylesheet=document.createElement('link')
-uiPolishStylesheet.rel='stylesheet'
-uiPolishStylesheet.href='/ui-polish.css?v=20260912'
-document.head.appendChild(uiPolishStylesheet)
 
 function Root(){return window.location.pathname==='/auth/setup-password'?<SetupPassword/>:<OperationalApp/>}
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><AuthProvider><Root/></AuthProvider></BrowserRouter></React.StrictMode>)
